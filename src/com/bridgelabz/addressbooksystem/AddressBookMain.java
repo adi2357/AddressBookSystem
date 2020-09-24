fe64e86c4706066e62e1d7aca35891c24862e292
Aditya Verma <aaadarths2357@gmail.com>
2020-09-25 01:43:37 +0530

    Use Case 1 : Create new Contact in Address Book

--- src/com/bridgelabz/addressbooksystem/AddressBookMain.java
+++ src/com/bridgelabz/addressbooksystem/AddressBookMain.java
@@ -1,8 +1,11 @@
 package com.bridgelabz.addressbooksystem;
+import java.util.Scanner;
 
 public class AddressBookMain {
 	public static void main(String[] args) {
-		System.out.println("Welcome to Address Book Program");	
+		
+		Contacts contacts=new Contacts("Aditya", "Verma", "3/4 LDA Colony", "Lucknow", "Uttar Pradesh",224045, "8803036140","address@gmail.com");
+		System.out.println(contacts.toString());		
 	}
 
 }
---
+++ src/com/bridgelabz/addressbooksystem/Contacts.java
@@ -0,0 +1,33 @@
+package com.bridgelabz.addressbooksystem;
+
+public class Contacts {
+	public String firstName;
+	public String lastName;
+	public String address;
+	public String city;
+	public String state;
+	public int zip;
+	public String phoneNumber;
+	public String email;
+	
+	public Contacts(String firstName, String lastName, String address, String city, String state, int zip,
+			String phoneNumber, String email) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.address = address;
+		this.city = city;
+		this.state = state;
+		this.zip = zip;
+		this.phoneNumber = phoneNumber;
+		this.email = email;
+	}
+	@Override
+    public String toString() {
+        return ("Name : "+firstName+" "+lastName+"\n"+"Address : "+ address+"\n"+"City : "+ city+"\n"+"State : "+state+"\n"+"Zip : "+zip+"\n"+"Phone Number : "+phoneNumber+"\n"+
+        		"Email : "+ email);
+    }
+	
+	
+	
+
+}
