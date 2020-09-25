033be61609899a18a2d97a07973b8bbe875ee1fc
Aditya Verma <aaadarths2357@gmail.com>
2020-09-25 12:24:48 +0530

    Use Case 2 : Add new Contact to Address Book

--- src/com/bridgelabz/addressbooksystem/AddressBookMain.java
+++ src/com/bridgelabz/addressbooksystem/AddressBookMain.java
@@ -1,11 +1,68 @@
 package com.bridgelabz.addressbooksystem;
+
+import java.util.TreeMap;
+import java.util.Map;
+import java.util.TreeMap;
 import java.util.Scanner;
 
 public class AddressBookMain {
-	public static void main(String[] args) {
+
+	private Map<String, Contacts> addressBook = new TreeMap<String, Contacts>();
+
+	public void addContactToAddressBook(Contacts contact) {
+		addressBook.put(contact.getFullName(), contact);
+	}
+
+	public void displayAddressBook() {
+		for(Map.Entry<String, Contacts> contact : addressBook.entrySet()) {
+			System.out.println(contact.getValue());			
+		}
 		
-		Contacts contacts=new Contacts("Aditya", "Verma", "3/4 LDA Colony", "Lucknow", "Uttar Pradesh",224045, "8803036140","address@gmail.com");
-		System.out.println(contacts.toString());		
+	}
+
+	public Contacts createContact() {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter First Name:");
+		String firstName = sc.next();
+		System.out.println("Enter Last Name:");
+		String lastName = sc.next();
+		System.out.println("Enter address:");
+		String address = sc.next();
+		System.out.println("Enter city:");
+		String city = sc.next();
+		System.out.println("Enter state:");
+		String state = sc.next();
+		System.out.println("Enter zip:");
+		int zip = sc.nextInt();
+		System.out.println("Enter phone No.:");
+		String phoneNumber = sc.next();
+		System.out.println("Enter email address:");
+		String email = sc.next();
+		Contacts newContact = new Contacts(firstName, lastName, address, city, state, zip, phoneNumber, email);
+		sc.close();
+		return newContact;
+	}
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		AddressBookMain addressBookObject = new AddressBookMain();
+		System.out.println("Create and Add Contact : ");
+		String choice = sc.next();
+		switch (choice) {
+		case "Y":
+			Contacts newContact = addressBookObject.createContact();
+			addressBookObject.addContactToAddressBook(newContact);
+			break;
+		case "N":
+			System.out.println("Aborting Operation");
+			break;
+		default:
+			System.out.println("Invalid Choice");
+		}
+		addressBookObject.displayAddressBook();
+
+		sc.close();
 	}
 
 }
--- src/com/bridgelabz/addressbooksystem/Contacts.java
+++ src/com/bridgelabz/addressbooksystem/Contacts.java
@@ -1,14 +1,14 @@
 package com.bridgelabz.addressbooksystem;
 
 public class Contacts {
-	public String firstName;
-	public String lastName;
-	public String address;
-	public String city;
-	public String state;
-	public int zip;
-	public String phoneNumber;
-	public String email;
+	private String firstName;
+	private String lastName;
+	private String address;
+	private String city;
+	private String state;
+	private int zip;
+	private String phoneNumber;
+	private String email;
 	
 	public Contacts(String firstName, String lastName, String address, String city, String state, int zip,
 			String phoneNumber, String email) {
@@ -21,9 +21,15 @@ public class Contacts {
 		this.phoneNumber = phoneNumber;
 		this.email = email;
 	}
+	
+	public String getFullName() {
+		return (firstName+" "+lastName);
+	}
+
+	
 	@Override
     public String toString() {
-        return ("Name : "+firstName+" "+lastName+"\n"+"Address : "+ address+"\n"+"City : "+ city+"\n"+"State : "+state+"\n"+"Zip : "+zip+"\n"+"Phone Number : "+phoneNumber+"\n"+
+        return ("Name : "+firstName+" "+lastName+"\t"+"Address : "+ address+"\t"+"City : "+ city+"\t"+"State : "+state+"\t"+"Zip : "+zip+"\t"+"Phone Number : "+phoneNumber+"\t"+
         		"Email : "+ email);
     }
 	
